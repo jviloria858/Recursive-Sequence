48372eee3751bd7993f93d5bb7f20c620c203531
jviloria858 <37822468+jviloria858@users.noreply.github.com>
2018-03-26 20:30:25 -0700

    comment update
    comment update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 /*
+A function f is defined as follows F(n)= (1) +(2*3) + (4*5*6) ... n. Given an integer n the task is to print the F(n)th term.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test contains an integer n.
+
+Output:
+For each test case print the nth term of the sequence. .
+
+Constraints:
+1<=T<=100
+1<=N<=10
+
 1	2x3	4x5x6	7x8x9x10	11x12x13x14x15	
 1	6	120	    5040	    360360	365527
 1	2	3	    4	        5	
